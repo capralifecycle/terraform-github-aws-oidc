bf1d5e40872dd705da3be110f903cf8fb8a7ae8a
Joakim L. Engeset <joakim.engeset@gmail.com>
2025-06-04 16:52:38 +0200

    tf: add empty terraform configuration

---
+++ versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.11.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.98.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.1.0"
+    }
+  }
+}
